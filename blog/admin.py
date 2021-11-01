f376d6ff46a5f146f0dcd62a3f436a841df35efb
gpfforte <gpf_forte@hotmail.com>
2021-11-01 20:49:54 +0100

    updated post section

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,6 @@ admin.site.register(Post)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'author',  'published_date', 'published')
-    list_filter = ('published', 'published_date')
+    list_display = ('post', 'author',  'date_published', 'published')
+    list_filter = ('published', 'date_published')
     search_fields = ('author', 'email', 'content')
--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -19,11 +20,12 @@ class Comment(models.Model):
                              on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=True)
 
     class Meta:
-        ordering = ('-published_date',)
+        ordering = ('-date_published',)
 
     def __str__(self):
         return f'Comment by {self.author}'
