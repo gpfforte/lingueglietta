7d7981c751f5a2c2340689b3502eb4f7fa671454
Gianpietro Forte <gpf_forte@hotmail.com>
2021-11-16 22:47:16 +0100

    Prova git

--- blog/form.py
+++ blog/form.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Comment
 from django.utils.translation import gettext_lazy as _
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
